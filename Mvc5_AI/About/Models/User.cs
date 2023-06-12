d7ba20c5f6e8743c85fcb4a0349c6eaa3e54c7f7
b10902100 <b10902100@ms.chu.edu.tw>
2023-06-12 21:41:33 +0800

    註冊登入介面

---
+++ Mvc5_AI/About/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace About.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirmed { get; set; }
+        
+
+    }
+}
