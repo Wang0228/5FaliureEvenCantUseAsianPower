62ccff9cd21d5d39a9f17b8ff4a9b845674f7468
ObsDIan <a29879350@gmail.com>
2023-06-28 17:08:52 +0800

    刪除多餘

--- Net7_AI_API/openAPI/ViewModels/SimilarModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace openAPI.ViewModels
-{
-    public class SimilarModel
-    {
-        public required string ChatId { get; set; }
-        public required string Question { get; set; }
-        public required string DataId { get; set; }
-    }
-}
--- Net7_AI_API/openAPI/ViewModels/TurboModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace openAPI.ViewModels
-{
-    public class TurboModel
-    {
-        public required string Question { get; set; }
-        public required string Sim_Anser { get; set; }
-        public required string ChatId{ get; set; }
-    }
-}
