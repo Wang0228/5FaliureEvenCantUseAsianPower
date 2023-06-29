97d39c2a44c22da1c1e1c3748c833f3aacff48ca
Wang <as258652000@yahoo.com.tw>
2023-06-30 00:30:46 +0800

    增加註解,以及資料庫無資料的例外狀況回傳 將api與端點提出 將練習用參數 調整為實際資料庫數值

--- openAPI/Services/AnswerService.cs
+++ openAPI/Services/AnswerService.cs
@@ -1,29 +1,31 @@
 ﻿using Azure.AI.OpenAI;
 using Azure;
-using openAPI.Interfaces;
 using openAPI.ViewModels;
 using openAPI.Models;
 using Microsoft.CodeAnalysis;
 
 namespace openAPI.Services
 {
-    public class AnswerService : IChatInterface
+    public class AnswerService 
     {
         private readonly HKContext _hkcontext;
+        string endpoint = "https://hacker6.openai.azure.com/";
+        string API_Key = "50520d3c1ceb4e79aefe93a6550b9eba";
         public AnswerService(HKContext hkcontext)
         {
             _hkcontext = hkcontext;
+            
         }
         public List<float> Embedding(string q)
-        {
-            OpenAIClient client = new(new Uri("https://hacker6.openai.azure.com/"), new AzureKeyCredential("50520d3c1ceb4e79aefe93a6550b9eba"));
+        {   
+            OpenAIClient client = new(new Uri(endpoint), new AzureKeyCredential(API_Key));
             var options = new EmbeddingsOptions(q);
             List<float> client_embedding = client.GetEmbeddings("embedding", options).Value.Data[0].Embedding.ToList();
             return client_embedding;
         }
         public string TurboChat(TurboModel msg)
         {
-            OpenAIClient client = new OpenAIClient(new Uri("https://hacker6.openai.azure.com/"), new AzureKeyCredential("50520d3c1ceb4e79aefe93a6550b9eba"));
+            OpenAIClient client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(API_Key));
 
             List<Qahistory> qahistory = _hkcontext.Qahistories.Where(x => x.ChatId == msg.ChatId).OrderByDescending(y => y.QahistoryId).Take(3).Reverse().ToList();
             IList<ChatMessage> messages = new List<ChatMessage>();
@@ -32,8 +34,8 @@ namespace openAPI.Services
             {
                 Messages = { new ChatMessage(ChatRole.System, @"你是藍星金流的客服人員") ,
                                     new ChatMessage(ChatRole.Assistant, $"你是一個客服人員,問題不知道或不相關請回答\"無相關資料,請在營業時間聯絡客服人員\",你只能參照「」內的內容回答問題「{ msg.Sim_Anser}」.只根據「」內的內容回答下面問題不要添加任何其他資訊:"),},
-                Temperature = (float)0.7,
-                MaxTokens = 350,
+                Temperature = msg.temperature,
+                MaxTokens = int.Parse(msg.Setting.Parameter),
                 NucleusSamplingFactor = (float)0.95,
                 FrequencyPenalty = 0,
                 PresencePenalty = 0,
@@ -44,7 +46,7 @@ namespace openAPI.Services
                     options.Messages.Add(new ChatMessage(ChatRole.Assistant, content.QahistoryA));
                 };
             options.Messages.Add(new ChatMessage(ChatRole.User, msg.Question));
-            Response<ChatCompletions> responseWithoutStream = client.GetChatCompletionsAsync("gpt-35-turbo", options).GetAwaiter().GetResult();//azure的模型部屬名稱
+            Response<ChatCompletions> responseWithoutStream = client.GetChatCompletionsAsync("gpt-35-turbo", options).GetAwaiter().GetResult();
 
             string completions = responseWithoutStream.Value.Choices[0].Message.Content.ToString();
 
@@ -52,11 +54,9 @@ namespace openAPI.Services
         }
         public string OtherChat(TurboModel msg)
         {
-            OpenAIClient client = new OpenAIClient(new Uri("https://hacker6.openai.azure.com/"), new AzureKeyCredential("50520d3c1ceb4e79aefe93a6550b9eba"));
+            OpenAIClient client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(API_Key));
 
             List<Qahistory> qahistory = _hkcontext.Qahistories.Where(x => x.ChatId == msg.ChatId).OrderByDescending(y => y.QahistoryId).Take(3).Reverse().ToList();
-
-            List<string> promptBuilder = new List<string>();
 
             string prompt = "";
             prompt += $"問題不知道或不相關請回答\"無相關資料,請在營業時間聯絡客服人員\",你只能參照「」內的內容回答問題「{msg.Sim_Anser}」.只根據「」內的內容回答下面問題不要添加任何其他資訊:";
@@ -72,7 +72,7 @@ namespace openAPI.Services
 
             CompletionsOptions options = new CompletionsOptions()
             {
-                MaxTokens = 1000,
+                MaxTokens = int.Parse(msg.Setting.Parameter),
                 Temperature = msg.temperature,
                 Prompts = {$"{prompt}"},
             };
